4193dee98ba15576873ddcdee2b32ab0ea8f5b56
Daria <kukuruzadasha86@gmail.com>
2023-02-23 15:04:22 +0200

    add

--- Bankomat/Program.cs
+++ Bankomat/Program.cs
@@ -10,118 +10,289 @@ namespace BankomatHW
     {
         public class BankAccount
         {
-            Money money = new Money();
             public string FullName { get; set; }
             public string AccountNumber { get; set; }
-            public BankAccount(string accountNumber, string fullName, string currency)
+            public string PinCode { get; set; }
+            public BankAccount(string accountNumber, string fullName, string pinCode)
             {
                 AccountNumber = accountNumber;
-                money.Balance = 0.0;
-                money.Currency = currency;
                 FullName = fullName;
+                PinCode = pinCode;
             }
-            public void Refill(double amount)
+            public BankAccount()
             {
-                money.Balance += amount;
+                AccountNumber = null;
+                FullName = null;
+                PinCode = null;
             }
-            public void Pay(double amount)
+        }
+        ///
+        public interface IMoney
+        {
+            void Refill();
+            void Pay();
+            double ShowPrice();
+            string ShowCorrency();
+            double ShowBalance();
+            void Convert(IMoney money);
+        }
+        public class USD: IMoney
+        {
+            public double Balance { get; set; }
+            public string Currency { get; set; }
+            public double Price { get; set; }
+            public USD(double balance, double price)
             {
-                if (money.Balance >= amount)
+                Balance = balance;
+                Currency = "USD";
+                Price = price;
+            }
+            public USD()
+            {
+                Balance = 0;
+                Currency = "USD";
+                Price = 0;
+            }
+            public void Refill()
+            {
+                Balance += Price;
+            }
+            public void Pay()
+            {
+                if (Balance >= Price)
                 {
-                    money.Balance -= amount;
+                    Balance -= Price;
                 }
                 else
                 {
                     throw new ArgumentException("Недостаточно денег");
                 }
             }
-            public double GetBalance()
+
+            public double ShowPrice()
             {
-                return money.Balance;
+                return Price;
             }
-            public string GetCorrency()
+            public string ShowCorrency()
             {
-                return money.Currency;
+                return Currency;
             }
 
+            public void Convert(IMoney money)
+            {
+                EUR eur = new EUR();
+                if (money.ShowCorrency() == eur.ShowCorrency())
+                {
+                    Price *= 0.94;
+                    Currency = eur.ShowCorrency();
+                }
+                else
+                {
+                    Price *= 36.74;
+                    Currency = "UAH";
+                }
+            }
+
+            public double ShowBalance()
+            {
+                return Balance;
+            }
         }
-        public class Money
+        public class EUR : IMoney
         {
             public double Balance { get; set; }
             public string Currency { get; set; }
-            public Money()
-            {
-                Currency = null;
-                Balance = 0;
-            }
-            public Money(string currency, double balance)
+            public double Price { get; set; }
+            public EUR(double balance, double price)
             {
                 Balance = balance;
-                Currency = currency;
+                Currency = "EUR";
+                Price = price;
             }
-        }
-        public interface IPin
-        {
-            int PinCode(int pin);
-        }
-        abstract public class ICheck
-        {
-            abstract public void Print();
-            virtual public void PrintCheck()
+            public EUR()
             {
+                Balance = 0;
+                Currency = "EUR";
+                Price = 0;
+            }
+            public void Refill()
+            {
+                Balance += Price;
+            }
+            public void Pay()
+            {
+                if (Balance >= Price)
+                {
+                    Balance -= Price;
+                }
+                else
+                {
+                    throw new ArgumentException("Недостаточно денег");
+                }
+            }
+            public double ShowPrice()
+            {
+                return Price;
+            }
+            public string ShowCorrency()
+            {
+                return Currency;
+            }
+            public double ShowBalance()
+            {
+                return Balance;
+            }
+            public void Convert(IMoney money)
+            {
+                USD usd = new USD();
+                if(money.ShowCorrency() == usd.ShowCorrency())
+                {
+                    Price *= 1.06;
+                    Currency = usd.ShowCorrency() ;
+                }
+                else
+                {
+                    Price *= 0.027;
+                    Currency = "UAH";
+                }
+            }
 
+          
+        }
+        public class UAH : IMoney
+        {
+            public double Balance { get; set; }
+            public string Currency { get; set; }
+            public double Price { get; set; }
+
+            public UAH(double balance, double price)
+            {
+                Balance = balance;
+                Currency = "UAH";
+                Price = price;
+            }
+            public UAH()
+            {
+                Balance = 0;
+                Currency = "UAH";
+                Price = 0;
+            }
+            public void Refill()
+            {
+                Balance += Price;
+            }
+            public void Pay()
+            {
+                if (Balance >= Price)
+                {
+                    Balance -= Price;
+                }
+                else
+                {
+                    throw new ArgumentException("Недостаточно денег");
+                }
+            }
+            public double ShowPrice()
+            {
+                return Price;
+            }
+            public string ShowCorrency()
+            {
+                return Currency;
+            }
+            public double ShowBalance()
+            {
+                return Balance;
+            }
+
+            public void Convert(IMoney money)
+            {
+                USD usd = new USD();
+                if (money.ShowCorrency() == usd.ShowCorrency())
+                {
+                    Price *= 0.027;
+                    Currency = usd.ShowCorrency();
+                }
+                else
+                {                    
+                    Price *= 0.026;
+                    Currency = "EUR";
+                }
             }
         }
-        class Bankomat:ICheck, IPin 
+        ///
+        public interface IPaymentInformation
         {
-            public int Pin { get; set; }
-            private BankAccount account;
-            public double Spent { get; set; }
-            public Bankomat(BankAccount account)
+            void Print(BankAccount account, IMoney money);
+        }
+        class SMS : IPaymentInformation
+        {
+            public string YourPhone { get; set; }
+            public SMS(string yourPhone)
             {
-                this.account = account;
-                Spent = 0.0;
+                YourPhone = yourPhone;
             }
-            public void Refill(double amount)
+            public void Print(BankAccount account, IMoney money)
             {
-                account.Refill(amount);
+                DateTime currentTime = DateTime.Now;
+                Console.WriteLine($"Почта: {YourPhone}\nИмя покупателя {account.FullName}\nTранзакция в размере: {money.ShowPrice()} {money.ShowCorrency()}" +
+                    $"\nВремя транзакции: {currentTime.ToString("HH:mm:ss")}");
             }
-            public void Pay(double amount)
+        }
+        class MailCheck : IPaymentInformation
+        {
+            public string YourEmail { get; set; }
+
+            public MailCheck(string yourEmail)
             {
-                account.Pay(amount);                
-                Spent = amount;
+                YourEmail = yourEmail;
             }
-            public double GetBalance()
+            public void Print(BankAccount account, IMoney money)
             {
-                return account.GetBalance();
+                DateTime currentTime = DateTime.Now;
+                Console.WriteLine($"Номер телефона: {YourEmail}\nИмя покупателя {account.FullName}\nTранзакция в размере: {money.ShowPrice()} {money.ShowCorrency()}" +
+                    $"\nВремя транзакции: {currentTime.ToString("HH:mm:ss")}");
             }
-            public string GetCurrency()
+        }
+        class PrintCheck : IPaymentInformation
+        {
+            public void Print(BankAccount account, IMoney money)
             {
-                return account.GetCorrency();
+                DateTime currentTime = DateTime.Now;
+                Console.WriteLine($"Имя покупателя {account.FullName}\nTранзакция в размере: {money.ShowPrice()} {money.ShowCorrency()}" +
+                    $"\nВремя транзакции: {currentTime.ToString("HH:mm:ss")}");
             }
-            public int PinCode(int pin)
+        }
+        ///
+        class Bankomat
+        {   
+            public void Print(BankAccount account, IMoney money,IPaymentInformation payment)
             {
-                return Pin = pin;
-            }           
-            public void PrintCheck()
-            {
-                Console.WriteLine($"Вы потратили: {Spent}\nБаланс: {GetBalance()} {GetCurrency()}");
+                payment.Print(account, money);
             }
-            public override void Print()
+            public void PrintBalancy(IMoney money)
             {
-                Console.WriteLine($"ФИО: {account.FullName}\nНомер карты: {account.AccountNumber}\nБаланс: {GetBalance()} {GetCurrency()}");
+                money.ShowBalance();
+            }
+            public void Convert(IMoney money, IMoney money2)
+            {
+                money.Convert(money2);
             }
         }
 
         static void Main(string[] args)
         {
-            BankAccount account = new BankAccount("12345674222324555556", "Daria Kukuruza", "UAN");
-            Bankomat bankomat = new Bankomat(account);
-            bankomat.Refill(1200);
-            bankomat.Print();
-            bankomat.Pay(34.56);
-            bankomat.PrintCheck();
+            BankAccount account = new BankAccount("123 456 742 223 245 555", "Daria Kukuruza", "14-53");
+            UAH money = new UAH(2000, 50);
 
+            PrintCheck check = new PrintCheck();
+            Bankomat bankomat = new Bankomat();
+            bankomat.Print(account, money, check);
+
+            EUR eur = new EUR();
+            bankomat.Convert(money,eur);
+
+            bankomat.Print(account, money, check);
         }
     }
 }
